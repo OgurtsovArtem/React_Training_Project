2e4ffd84f018100221d6341c081d6c54178df3ef
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-03-11 13:30:01 +0700

    added routes to other pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom';
+// PAGES
 import Main from '../pages/Main';
+import Blog from '../pages/Blog';
+import AboutUs from '../pages/AboutUs';
+import Equipment from '../pages/Equipment';
 import NotFound404 from './NotFound404/NotFound404'
 
 import './App.css';
@@ -8,8 +12,17 @@ import './App.css';
 function App() {
   return (
     <Switch>
-    <Route exact path="/">
+      <Route exact path="/">
         <Main />
+      </Route>
+      <Route  path="/about-us">
+        <AboutUs />
+      </Route>
+      <Route  path="/blog">
+        <Blog />
+      </Route>
+      <Route  path="/equipment">
+        <Equipment />
       </Route>
       <Route path="*">
         <NotFound404 />
